5a98630437d11a201e7f7802105d223407fc9615
Star Agent <star@agent.com>
2026-01-10 17:05:33 -0300

    feat: replace with borderless tarot cards + revert to object-fit cover

---
+++ copy_cards.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# Directorio source y destino
+SRC="/home/star/Descargas/tarot_downloads/sin_borde"
+DEST="/home/star/star/public/cards"
+
+# Arcanos Mayores (00-21)
+cp "$SRC/RWSa-T-00.png" "$DEST/ar00.jpg"
+cp "$SRC/RWSa-T-01.png" "$DEST/ar01.jpg"
+cp "$SRC/RWSa-T-02.png" "$DEST/ar02.jpg"
+cp "$SRC/RWSa-T-03.png" "$DEST/ar03.jpg"
+cp "$SRC/RWSa-T-04.png" "$DEST/ar04.jpg"
+cp "$SRC/RWSa-T-05.png" "$DEST/ar05.jpg"
+cp "$SRC/RWSa-T-06.png" "$DEST/ar06.jpg"
+cp "$SRC/RWSa-T-07.png" "$DEST/ar07.jpg"
+cp "$SRC/RWSa-T-08.png" "$DEST/ar08.jpg"
+cp "$SRC/RWSa-T-09.png" "$DEST/ar09.jpg"
+cp "$SRC/RWSa-T-10.png" "$DEST/ar10.jpg"
+cp "$SRC/RWSa-T-11.png" "$DEST/ar11.jpg"
+cp "$SRC/RWSa-T-12.png" "$DEST/ar12.jpg"
+cp "$SRC/RWSa-T-13.png" "$DEST/ar13.jpg"
+cp "$SRC/RWSa-T-14.png" "$DEST/ar14.jpg"
+cp "$SRC/RWSa-T-15.png" "$DEST/ar15.jpg"
+cp "$SRC/RWSa-T-16.png" "$DEST/ar16.jpg"
+cp "$SRC/RWSa-T-17.png" "$DEST/ar17.jpg"
+cp "$SRC/RWSa-T-18.png" "$DEST/ar18.jpg"
+cp "$SRC/RWSa-T-19.png" "$DEST/ar19.jpg"
+cp "$SRC/RWSa-T-20.png" "$DEST/ar20.jpg"
+cp "$SRC/RWSa-T-21.png" "$DEST/ar21.jpg"
+
+# Bastos (Wands)
+cp "$SRC/RWSa-W-0A.png" "$DEST/waac.jpg"  # As
+cp "$SRC/RWSa-W-02.png" "$DEST/wa02.jpg"
+cp "$SRC/RWSa-W-03.png" "$DEST/wa03.jpg"
+cp "$SRC/RWSa-W-04.png" "$DEST/wa04.jpg"
+cp "$SRC/RWSa-W-05.png" "$DEST/wa05.jpg"
+cp "$SRC/RWSa-W-06.png" "$DEST/wa06.jpg"
+cp "$SRC/RWSa-W-07.png" "$DEST/wa07.jpg"
+cp "$SRC/RWSa-W-08.png" "$DEST/wa08.jpg"
+cp "$SRC/RWSa-W-09.png" "$DEST/wa09.jpg"
+cp "$SRC/RWSa-W-10.png" "$DEST/wa10.jpg"
+cp "$SRC/RWSa-W-J1.png" "$DEST/wapa.jpg"  # Page
+cp "$SRC/RWSa-W-J2.png" "$DEST/wakn.jpg"  # Knight
+cp "$SRC/RWSa-W-QU.png" "$DEST/waqu.jpg"  # Queen
+cp "$SRC/RWSa-W-KI.png" "$DEST/waki.jpg"  # King
+
+# Copas (Cups)
+cp "$SRC/RWSa-C-0A.png" "$DEST/cuac.jpg"  # As
+cp "$SRC/RWSa-C-02.png" "$DEST/cu02.jpg"
+cp "$SRC/RWSa-C-03.png" "$DEST/cu03.jpg"
+cp "$SRC/RWSa-C-04.png" "$DEST/cu04.jpg"
+cp "$SRC/RWSa-C-05.png" "$DEST/cu05.jpg"
+cp "$SRC/RWSa-C-06.png" "$DEST/cu06.jpg"
+cp "$SRC/RWSa-C-07.png" "$DEST/cu07.jpg"
+cp "$SRC/RWSa-C-08.png" "$DEST/cu08.jpg"
+cp "$SRC/RWSa-C-09.png" "$DEST/cu09.jpg"
+cp "$SRC/RWSa-C-10.png" "$DEST/cu10.jpg"
+cp "$SRC/RWSa-C-J1.png" "$DEST/cupa.jpg"  # Page
+cp "$SRC/RWSa-C-J2.png" "$DEST/cukn.jpg"  # Knight
+cp "$SRC/RWSa-C-QU.png" "$DEST/cuqu.jpg"  # Queen
+cp "$SRC/RWSa-C-KI.png" "$DEST/cuki.jpg"  # King
+
+# Espadas (Swords)
+cp "$SRC/RWSa-S-0A.png" "$DEST/swac.jpg"  # As
+cp "$SRC/RWSa-S-02.png" "$DEST/sw02.jpg"
+cp "$SRC/RWSa-S-03.png" "$DEST/sw03.jpg"
+cp "$SRC/RWSa-S-04.png" "$DEST/sw04.jpg"
+cp "$SRC/RWSa-S-05.png" "$DEST/sw05.jpg"
+cp "$SRC/RWSa-S-06.png" "$DEST/sw06.jpg"
+cp "$SRC/RWSa-S-07.png" "$DEST/sw07.jpg"
+cp "$SRC/RWSa-S-08.png" "$DEST/sw08.jpg"
+cp "$SRC/RWSa-S-09.png" "$DEST/sw09.jpg"
+cp "$SRC/RWSa-S-10.png" "$DEST/sw10.jpg"
+cp "$SRC/RWSa-S-J1.png" "$DEST/swpa.jpg"  # Page
+cp "$SRC/RWSa-S-J2.png" "$DEST/swkn.jpg"  # Knight
+cp "$SRC/RWSa-S-QU.png" "$DEST/swqu.jpg"  # Queen
+cp "$SRC/RWSa-S-KI.png" "$DEST/swki.jpg"  # King
+
+# Pentáculos (Pentacles)
+cp "$SRC/RWSa-P-0A.png" "$DEST/peac.jpg"  # As
+cp "$SRC/RWSa-P-02.png" "$DEST/pe02.jpg"
+cp "$SRC/RWSa-P-03.png" "$DEST/pe03.jpg"
+cp "$SRC/RWSa-P-04.png" "$DEST/pe04.jpg"
+cp "$SRC/RWSa-P-05.png" "$DEST/pe05.jpg"
+cp "$SRC/RWSa-P-06.png" "$DEST/pe06.jpg"
+cp "$SRC/RWSa-P-07.png" "$DEST/pe07.jpg"
+cp "$SRC/RWSa-P-08.png" "$DEST/pe08.jpg"
+cp "$SRC/RWSa-P-09.png" "$DEST/pe09.jpg"
+cp "$SRC/RWSa-P-10.png" "$DEST/pe10.jpg"
+cp "$SRC/RWSa-P-J1.png" "$DEST/pepa.jpg"  # Page
+cp "$SRC/RWSa-P-J2.png" "$DEST/pekn.jpg"  # Knight
+cp "$SRC/RWSa-P-QU.png" "$DEST/pequ.jpg"  # Queen
+cp "$SRC/RWSa-P-KI.png" "$DEST/peki.jpg"  # King
+
+echo "✅ Todas las cartas copiadas exitosamente"
